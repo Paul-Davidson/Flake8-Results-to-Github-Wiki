4b6ffbfd2dd1b5a1fc674a4987bea39ce55936cc
Paul Davidson <pauldavidson@protonmail.com>
2024-01-14 17:45:12 +1100

    Added git actor and email

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,13 @@ echo $flake8_outputs
 python /entrypoint.py --path-to-wiki-repo $2 --data "$flake8_output"
 cp output.md "$2/${repo_name}-Flake8-Report.md"
 
-# cat "$2/${repo_name}-Flake8-Report.md"
-
 echo $repo_name
 echo $GITHUB_ACTOR
 
 cd $2
 
-# git config --global user.name "${GITHUB_ACTOR}"
-# git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git add "${repo_name}-Flake8-Report.md"
 git commit -m "[bot] Created Flake8 Report for ${repo_name}" --allow-empty
