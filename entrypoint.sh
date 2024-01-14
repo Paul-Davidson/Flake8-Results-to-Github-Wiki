11c397c737d00d1935d6aafe9f39be1174182abd
Paul Davidson <pauldavidson@protonmail.com>
2024-01-14 17:19:37 +1100

    More testing 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,7 @@ echo $repo_name
 echo $GITHUB_ACTOR
 
 cd $2
+git config --global --add safe.directory $2
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add "${repo_name}-Flake8-Report.md"
