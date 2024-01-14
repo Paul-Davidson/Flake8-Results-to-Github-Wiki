f86ab2e2bb7202e3f42d2e7e2606e4fd78c3e89f
Paul Davidson <pauldavidson@protonmail.com>
2024-01-14 15:40:30 +1100

    Another test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo $1
 echo $2
-echo $flake8_output
 
 flake8_output=$(flake8 --format json $1)
-python /entrypoint.py --path-to-wiki-repo $2 --data $flake8_output
+echo $flake8_output
+
+python /entrypoint.py --path-to-wiki-repo $2 --data "$flake8_output"
