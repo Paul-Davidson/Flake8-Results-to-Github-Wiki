f86d082e262eeb19bc99ace0f927c186d4d83938
Paul Davidson <pauldavidson@protonmail.com>
2024-01-14 17:27:51 +1100

    Fix safe directory git

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ echo $repo_name
 echo $GITHUB_ACTOR
 
 cd $2
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --add safe.directory "$GITHUB_WORKSPACE/$2"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
